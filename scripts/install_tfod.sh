5ab7d6b512a2188af10efe56766056c808c284af
Behnam Moradi <47978272+Bmoradi93@users.noreply.github.com>
2022-03-10 15:32:05 -0800

    Update install_tfod.sh

--- scripts/install_tfod.sh
+++ scripts/install_tfod.sh
@@ -3,9 +3,9 @@
 cd /home/tensorflow/models/research/
 
 # install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py
 python3 get-pip.py --force-reinstall
-
+python3 -m pip install --user --upgrade pip
 # Compile protos.
 protoc object_detection/protos/*.proto --python_out=.
 
@@ -15,7 +15,7 @@ python3 -m pip install --use-feature=2020-resolver .
 
 pip install labelImg
 
-apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+apt-get install -y '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev vim tmux
 
 # Test the installation.
 python3 object_detection/builders/model_builder_tf1_test.py
